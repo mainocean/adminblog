35fbc5976ef07597bb4f238f5dc5f18a01c4c8e6
mainocean <mainoceanm@gmail.com>
2024-06-28 22:02:57 +0300

    alex
    2 push

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-01-03 20:26:03 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-01-03T20:26:03+00:00	INFO 188.163.101.31	joomlafailure	Такий користувач не існує або у вас відсутній обліковий запис.
